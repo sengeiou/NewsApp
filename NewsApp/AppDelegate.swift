5af7a37d6b8ba5a77f63570ae6d70599aebf4ef3
ThanhCong Nguyen <o00ontcong@gmail.com>
2020-08-27 15:36:23 +0700

    set badget

--- NewsApp/AppDelegate.swift
+++ NewsApp/AppDelegate.swift
@@ -10,16 +10,16 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         self.window = UIWindow(frame: UIScreen.main.bounds)
         setUpWindow(window)
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
     @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
@@ -33,9 +33,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        UIApplication.shared.applicationIconBadgeNumber = 99
+
+    }
     func setUpWindow(_ window: UIWindow?) {
         window?.rootViewController = TabBarViewController(nib: R.nib.tabBarViewController)
-       
+        
         window?.makeKeyAndVisible()
     }
 }
